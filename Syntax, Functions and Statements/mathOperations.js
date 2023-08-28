05d5648a6c0023d7e7fe02b967ab0f04ede303bd
Valeriya Petkova <valeriya-petkova@abv.bg>
2023-08-28 21:24:45 +0300

    Add tasks

---
+++ Syntax, Functions and Statements/mathOperations.js
@@ -0,0 +1,18 @@
+function mathOperations(num1, num2, sign) {
+    let result = 0;
+
+    if (sign === '+') {
+        result = num1 + num2;
+    } else if (sign === '-') {
+        result = num1 - num2;
+    } else if (sign === '*') {
+        result = num1 * num2;
+    } else if (sign === '/') {
+        result = num1 / num2;
+    } else if (sign === '%') {
+        result = num1 % num2;
+    } else if (sign === '**') {
+        result = num1 ** num2;
+    }
+    console.log(result);
+}
