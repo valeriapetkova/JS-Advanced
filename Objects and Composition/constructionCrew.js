fe00ad7a589f2ebda261164499e2fed2ebd6902c
Valeriya Petkova <valeriya-petkova@abv.bg>
2023-09-01 19:53:22 +0300

    Create constructionCrew.js

---
+++ Objects and Composition/constructionCrew.js
@@ -0,0 +1,9 @@
+function constructionCrew(worker) {
+
+    if (worker.dizziness) {
+        worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
+        worker.dizziness = false;
+    }
+
+    return worker;
+}
